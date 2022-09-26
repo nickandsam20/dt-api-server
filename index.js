7ac779f1dd34da4e8a7ff0a320ad574f5304d915
sam <sam990126@gmail.com>
2022-09-26 10:10:14 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let express=require('express');
+let greenArea=require("./apis/greenArea.js");
+let port = process.env.PORT || 3000;
+
+
+let app=express();
+app.use("/api/",greenArea);
+app.get('/', function (req, res) {
+    res.send('Hello')
+})
+
+app.listen(3000,(err)=>{
+	if(err){
+		console.log("init server error",err);
+	}else{
+		console.log("listen on PORT",port);
+	}
+});
